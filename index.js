7562cefe1171ce77b6bb0401c7f29fecc0e5c59a
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-06-13 22:19:11 +0900

    Modify: 코드 일부 수정

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import express from "express";
 import dotenv from "dotenv";
-
+import express from "express";
 import root from "./router/root";
 import movie from "./router/movie";
 
 dotenv.config();
-const app = express();
+
 const PORT = 8080;
+const app = express();
 
 app.use("/", root);
 app.use("/movie", movie);
+
 app.listen(PORT, () => console.log(`Run server on ${PORT}`));
